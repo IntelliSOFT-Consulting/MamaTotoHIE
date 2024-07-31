9016d0ea6971bf8e0c9d200daaa834ab0d789731
menyagah <menyagah27@gmail.com>
2024-08-01 02:03:23 +0300

    Remove the unused folder module entry point

--- terraform_/main.tf
+++ terraform_/main.tf
@@ -1,11 +1,7 @@
-module "folders" {
-    source = "./folders"
-    project_id = "hdc-test-arena"
-}
-
 module "compute" {
     source = "./compute"
     project_name = "hdc-test-arena"
+    project_id = "hdc-test-arena"
 }
 
 module "network" {
