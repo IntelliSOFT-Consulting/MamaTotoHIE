90a615e56e59637d7b5e5c13b672c5475c8f270f
menyagah <menyagah27@gmail.com>
2024-08-01 01:33:52 +0300

    Add network module

---
+++ terraform_/Network/variable.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  type        = string
+  description = "the initial project name and project id"
+}
+
+variable "project_network" {
+  type        = string
+  description = "the initial project name and project id"
+}
