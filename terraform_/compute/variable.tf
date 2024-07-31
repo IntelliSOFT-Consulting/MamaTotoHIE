9cc92d2e31d87b1596ac31cc98b7b9483c9eef83
menyagah <menyagah27@gmail.com>
2024-08-01 02:01:25 +0300

    Move the compute instance code to the compute folder

--- terraform_/compute/variable.tf
+++ terraform_/compute/variable.tf
@@ -2,3 +2,8 @@ variable "project_name" {
   type        = string
   description = "the initial project name and project id"
 }
+
+variable "project_id" {
+  type        = string
+  description = "the initial project name and project id"
+}
